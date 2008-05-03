4d9cf56da19c9031189f59ca6d9b642524c019d3
wgreen <wgreen@d21c1205-ac1a-0410-ae1a-9dc866783d85>
2008-05-03 10:54:24 +0000

    New configuration modules loads configuration from text file.

---
+++ perl/RackMonkey/Conf.pm
@@ -0,0 +1,38 @@
+package RackMonkey::Conf;
+##############################################################################
+# RackMonkey - Know Your Racks - http://www.rackmonkey.org                   #
+# Version 1.2.%BUILD%                                                        #
+# (C)2004-2008 Will Green (wgreen at users.sourceforge.net)                  #
+# Configuration loader for RackMonkey                                        #
+##############################################################################
+
+use strict;
+use warnings;
+
+use RackMonkey::Error;
+
+our $VERSION = '1.2.%BUILD%';
+our $AUTHOR = 'Will Green (wgreen at users.sourceforge.net)';
+
+our $conf;
+
+# load the config file 
+my $configFile = $ENV{'RACKMONKEY_CONF'} || '/etc/rackmonkey.conf';
+
+unless (open(CONFIG, "<$configFile"))
+{
+	my $errMsg = "RMERR: Cannot open configuration file '$configFile': $!. You can override the configuration file path with the RACKMONKEY_CONF environment variable.";
+	print "Content-type: text/html\n\n";
+	my $friendlyErrMsg = RackMonkey::Error::enlighten($errMsg);
+	RackMonkey::Error::display($errMsg, $friendlyErrMsg);
+}
+
+my %confHash = map 
+{
+  s/#.*//;		# comments
+  s/^\s+//;		# whitespace
+  s/\s+$//;		# 	"	"
+  m/\s*(.*?)\s*=\s*(.*)\s*/; 
+}
+<CONFIG>;
+$conf = \%confHash;
