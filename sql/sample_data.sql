bc21d8f93bd0dbfbddcee18f57347bb148a548d6
wgreen <wgreen@d21c1205-ac1a-0410-ae1a-9dc866783d85>
2008-06-07 12:30:39 +0000

    Expanded included operating systems.

--- sql/sample_data.sql
+++ sql/sample_data.sql
@@ -5,13 +5,19 @@
 -- Sample content for RackMonkey database                                   --
 ------------------------------------------------------------------------------
 
+-- The inclusion of a company or product in this file does not consitute an endorement by the author. 
+-- All trademarks are the property of their respective owners.
+
 -- sample organisations
 INSERT INTO org (name, account_no, customer, software, hardware, descript, home_page, notes, meta_update_time, meta_update_user) VALUES('Apple', 		NULL, 0, 1, 1, 'Apple', 'http://www.apple.com', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
 INSERT INTO org (name, account_no, customer, software, hardware, descript, home_page, notes, meta_update_time, meta_update_user) VALUES('Canonical', 	NULL, 0, 1, 0, 'Canonical', 'http://www.canonical.com', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
+INSERT INTO org (name, account_no, customer, software, hardware, descript, home_page, notes, meta_update_time, meta_update_user) VALUES('CentOS', 		NULL, 0, 1, 0, 'CentOS Project', 'http://www.centos.org', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
 INSERT INTO org (name, account_no, customer, software, hardware, descript, home_page, notes, meta_update_time, meta_update_user) VALUES('Cisco', 		NULL, 0, 1, 1, 'Cisco Systems', 'http://www.cisco.com', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
+INSERT INTO org (name, account_no, customer, software, hardware, descript, home_page, notes, meta_update_time, meta_update_user) VALUES('Debian', 		NULL, 0, 1, 0, 'Debian Project', 'http://www.debian.org', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
 INSERT INTO org (name, account_no, customer, software, hardware, descript, home_page, notes, meta_update_time, meta_update_user) VALUES('Dell', 		NULL, 0, 0, 1, 'Dell', 'http://www.dell.com', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
 INSERT INTO org (name, account_no, customer, software, hardware, descript, home_page, notes, meta_update_time, meta_update_user) VALUES('Foundry', 		NULL, 0, 0, 1, 'Foundry Networks', 'http://www.foundrynet.com', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
 INSERT INTO org (name, account_no, customer, software, hardware, descript, home_page, notes, meta_update_time, meta_update_user) VALUES('FreeBSD', 		NULL, 0, 1, 0, 'FreeBSD Project', 'http://www.freebsd.org', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
+INSERT INTO org (name, account_no, customer, software, hardware, descript, home_page, notes, meta_update_time, meta_update_user) VALUES('Gentoo', 		NULL, 0, 1, 0, 'Gentoo Foundation', 'http://www.gentoo.org', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
 INSERT INTO org (name, account_no, customer, software, hardware, descript, home_page, notes, meta_update_time, meta_update_user) VALUES('HP', 			NULL, 0, 1, 1, 'Hewlett Packard', 'http://www.hp.com', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
 INSERT INTO org (name, account_no, customer, software, hardware, descript, home_page, notes, meta_update_time, meta_update_user) VALUES('IBM', 			NULL, 0, 1, 1, 'International Business Machines', 'http://www.ibm.com', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
 INSERT INTO org (name, account_no, customer, software, hardware, descript, home_page, notes, meta_update_time, meta_update_user) VALUES('Juniper', 		NULL, 0, 1, 1, 'Juniper Networks', 'http://www.juniper.net', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
@@ -24,29 +30,36 @@ INSERT INTO org (name, account_no, customer, software, hardware, descript, home_
 INSERT INTO org (name, account_no, customer, software, hardware, descript, home_page, notes, meta_update_time, meta_update_user) VALUES('OpenBSD', 		NULL, 0, 1, 0, 'OpenBSD Project', 'http://www.openbsd.org', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
 INSERT INTO org (name, account_no, customer, software, hardware, descript, home_page, notes, meta_update_time, meta_update_user) VALUES('Red Hat', 		NULL, 0, 1, 0, 'Red Hat', 'http://www.redhat.com', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
 INSERT INTO org (name, account_no, customer, software, hardware, descript, home_page, notes, meta_update_time, meta_update_user) VALUES('Slackware',	NULL, 0, 1, 0, 'Slackware Linux Project', 'http://www.slackware.org', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
-INSERT INTO org (name, account_no, customer, software, hardware, descript, home_page, notes, meta_update_time, meta_update_user) VALUES('SPI', 			NULL, 0, 1, 0, 'Software in the Public Interest', 'http://www.spi-inc.org', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
 INSERT INTO org (name, account_no, customer, software, hardware, descript, home_page, notes, meta_update_time, meta_update_user) VALUES('Sun', 			NULL, 0, 1, 1, 'Sun Microsystems', 'http://www.sun.com', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
+INSERT INTO org (name, account_no, customer, software, hardware, descript, home_page, notes, meta_update_time, meta_update_user) VALUES('VMWare', 		NULL, 0, 1, 0, 'VMWare', 'http://www.vmware.com', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
+
 
 -- sample operating systems
-INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('AIX', 				10, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
-INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('Debian', 			21, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
-INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('RHEL', 				19, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
-INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('FreeBSD', 			8, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
-INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('HP-UX', 				9, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
-INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('IOS', 				5, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
-INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('Mac OS X Server',	3, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
-INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('NetBSD', 			16, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
-INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('OpenBSD',			18, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
-INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('Slackware Linux',	20, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
-INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('Solaris', 			22, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
-INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('SUSE Linux', 		17, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
-INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('Ubuntu Linux', 		4, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
-INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('Windows Server', 	14, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
+INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('AIX', 						13, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
+INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('CentOS', 					5, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
+INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('Debian', 					7, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
+INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('Fedora Core', 				22, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
+INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('FreeBSD', 					10, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
+INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('Gentoo Linux', 				11, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
+INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('HP-UX', 						12, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
+INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('IOS', 						6, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
+INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('JUNOS', 						14, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
+INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('Mac OS X Server',			3, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
+INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('NetBSD', 					19, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
+INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('OpenBSD',					21, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
+INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('OpenSolaris', 				24, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
+INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('Red Hat Enterprise Linux', 	22, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
+INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('Slackware Linux',			23, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
+INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('Solaris', 					24, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
+INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('SUSE Linux', 				20, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
+INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('Ubuntu',		 				4, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
+INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('VMware ESX Server', 			25, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
+INSERT INTO os (name, manufacturer, notes, meta_update_time, meta_update_user) VALUES('Windows Server', 			17, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
 
 -- sample hardware
-INSERT INTO hardware (name, manufacturer, size, image, support_url, spec_url, notes, meta_update_time, meta_update_user) VALUES('Catalyst 3560', 		5, 1, NULL, 'http://www.cisco.com/en/US/products/hw/switches/ps5528/tsd_products_support_series_home.html', 'http://www.cisco.com/en/US/products/hw/switches/ps5528/products_data_sheet09186a00801f3d7d.html', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
-INSERT INTO hardware (name, manufacturer, size, image, support_url, spec_url, notes, meta_update_time, meta_update_user) VALUES('Fire T2000', 			22, 2, NULL, 'http://sunsolve.sun.com/handbook_pub/Systems/SunFireT2000/SunFireT2000.html', 'http://sunsolve.sun.com/handbook_pub/Systems/SunFireT2000/spec.html', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
-INSERT INTO hardware (name, manufacturer, size, image, support_url, spec_url, notes, meta_update_time, meta_update_user) VALUES('PowerEdge 2850', 		6, 2, NULL, 'http://support.dell.com/support/edocs/systems/pe2850/en/', NULL, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
+INSERT INTO hardware (name, manufacturer, size, image, support_url, spec_url, notes, meta_update_time, meta_update_user) VALUES('Catalyst 3560', 		6, 1, NULL, 'http://www.cisco.com/en/US/products/hw/switches/ps5528/tsd_products_support_series_home.html', 'http://www.cisco.com/en/US/products/hw/switches/ps5528/products_data_sheet09186a00801f3d7d.html', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
+INSERT INTO hardware (name, manufacturer, size, image, support_url, spec_url, notes, meta_update_time, meta_update_user) VALUES('Fire T2000', 			24, 2, NULL, 'http://sunsolve.sun.com/handbook_pub/Systems/SunFireT2000/SunFireT2000.html', 'http://sunsolve.sun.com/handbook_pub/Systems/SunFireT2000/spec.html', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
+INSERT INTO hardware (name, manufacturer, size, image, support_url, spec_url, notes, meta_update_time, meta_update_user) VALUES('PowerEdge 2850', 		8, 2, NULL, 'http://support.dell.com/support/edocs/systems/pe2850/en/', NULL, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
 
 -- sample roles
 INSERT INTO role (name, descript, notes, meta_update_time, meta_update_user) VALUES('App Server', 			'Application Server', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
@@ -57,12 +70,13 @@ INSERT INTO role (name, descript, notes, meta_update_time, meta_update_user) VAL
 INSERT INTO role (name, descript, notes, meta_update_time, meta_update_user) VALUES('Dev Server', 			'Development Server', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
 INSERT INTO role (name, descript, notes, meta_update_time, meta_update_user) VALUES('FC Switch', 			'Fibre Channel Switch', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
 INSERT INTO role (name, descript, notes, meta_update_time, meta_update_user) VALUES('File Server', 			'File Server', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
+INSERT INTO role (name, descript, notes, meta_update_time, meta_update_user) VALUES('General Server', 		'General Purpose Server', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
 INSERT INTO role (name, descript, notes, meta_update_time, meta_update_user) VALUES('Human Interface', 		'Monitor/Keyboard etc.', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
-INSERT INTO role (name, descript, notes, meta_update_time, meta_update_user) VALUES('KVM', 					'KVM Server', 	'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
+INSERT INTO role (name, descript, notes, meta_update_time, meta_update_user) VALUES('KVM', 					'KVM Switch', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
 INSERT INTO role (name, descript, notes, meta_update_time, meta_update_user) VALUES('Monitoring', 			'Systems Monitoring', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
 INSERT INTO role (name, descript, notes, meta_update_time, meta_update_user) VALUES('Patch Panel', 			'Patch Panel', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
 INSERT INTO role (name, descript, notes, meta_update_time, meta_update_user) VALUES('Physical Storage', 	'Storage for Physical Items', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
-INSERT INTO role (name, descript, notes, meta_update_time, meta_update_user) VALUES('Power', 				'Electrical power infrastructure', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
+INSERT INTO role (name, descript, notes, meta_update_time, meta_update_user) VALUES('Power', 				'Electrical Power Infrastructure', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
 INSERT INTO role (name, descript, notes, meta_update_time, meta_update_user) VALUES('Router', 				'Network Router', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
 INSERT INTO role (name, descript, notes, meta_update_time, meta_update_user) VALUES('Streaming', 			'Streaming Audio/Video Server', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
 INSERT INTO role (name, descript, notes, meta_update_time, meta_update_user) VALUES('Switch', 				'Network Switch', 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
@@ -74,6 +88,3 @@ INSERT INTO building (name, name_short, notes, meta_update_time, meta_update_use
 INSERT INTO room (name, building, has_rows, notes, meta_update_time, meta_update_user) VALUES('101', 6, 0, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
 INSERT INTO row (name, room, room_pos, hidden_row, notes, meta_update_time, meta_update_user) VALUES('-', 6, 0, 1, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
 INSERT INTO rack (name, row, row_pos, hidden_rack, size, notes, meta_update_time, meta_update_user) VALUES('A1', 6, 0, 0, 42, 'Sample data included with RackMonkey.', '2007-01-01 00:00:00', 'install');
-
-
--- The inclusion of a company or product in this file does not consitute an endorement by the author.
