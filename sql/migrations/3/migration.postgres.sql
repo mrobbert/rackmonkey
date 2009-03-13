304569aae63b0dda5c9f4d09f11f43ebcee75e54
wgreen <wgreen@d21c1205-ac1a-0410-ae1a-9dc866783d85>
2009-03-13 13:52:53 +0000

    Started migration files for Postgres and MySQL.

---
+++ sql/migrations/3/migration.postgres.sql
@@ -0,0 +1,8 @@
+------------------------------------------------------------------------------
+-- RackMonkey - Know Your Racks - http://www.rackmonkey.org                 --
+-- Version 1.2.%BUILD%                                                      --
+-- (C)2004-2009 Will Green (wgreen at users.sourceforge.net)                --
+-- Update database schema to v3 for Postgres                                --
+------------------------------------------------------------------------------
+
+-- Add NOT NULL for all fk
