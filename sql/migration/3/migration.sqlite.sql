b20af4ebe803e0799c840d9a5ee800eca490be81
wgreen <wgreen@d21c1205-ac1a-0410-ae1a-9dc866783d85>
2009-03-15 13:10:54 +0000

    Minor comment change.

--- sql/migration/3/migration.sqlite.sql
+++ sql/migration/3/migration.sqlite.sql
@@ -2,7 +2,7 @@
 -- RackMonkey - Know Your Racks - http://www.rackmonkey.org                 --
 -- Version 1.2.%BUILD%                                                      --
 -- (C)2004-2009 Will Green (wgreen at users.sourceforge.net)                --
--- Update database SQLite schema to v3                                      --
+-- Update SQLite database schema to v3                                      --
 -- ---------------------------------------------------------------------------
 
 ALTER TABLE rack ADD COLUMN numbering_direction INTEGER NOT NULL DEFAULT 0;
