8211fe3c20f82b9255c79b36b1b8e9b2507e582d
wgreen <wgreen@d21c1205-ac1a-0410-ae1a-9dc866783d85>
2009-03-15 13:25:42 +0000

    Minor comment change to other migration files.

--- sql/migration/3/migration.mysql.sql
+++ sql/migration/3/migration.mysql.sql
@@ -2,5 +2,5 @@
 -- RackMonkey - Know Your Racks - http://www.rackmonkey.org                 --
 -- Version 1.2.%BUILD%                                                      --
 -- (C)2004-2009 Will Green (wgreen at users.sourceforge.net)                --
--- Update database schema to v3 for MySQL                                   --
+-- Update MySQL database schema to v3                                       --
 -- ---------------------------------------------------------------------------
--- sql/migration/3/migration.postgres.sql
+++ sql/migration/3/migration.postgres.sql
@@ -2,7 +2,7 @@
 -- RackMonkey - Know Your Racks - http://www.rackmonkey.org                 --
 -- Version 1.2.%BUILD%                                                      --
 -- (C)2004-2009 Will Green (wgreen at users.sourceforge.net)                --
--- Update database schema to v3 for Postgres                                --
+-- Update Postgres database schema to v3                                    --
 -- ---------------------------------------------------------------------------
 
 -- Add NOT NULL for all fk
