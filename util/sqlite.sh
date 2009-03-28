aaecf21121d82c267dc58d0246f24792920b9ff0
wgreen <wgreen@d21c1205-ac1a-0410-ae1a-9dc866783d85>
2009-03-28 16:28:39 +0000

    Adjusted to run from root of trunk.

--- util/sqlite.sh
+++ util/sqlite.sh
@@ -1,7 +1,7 @@
 DB_FILE=/Users/flux/Sites/data/rackmonkey.db
 rm $DB_FILE
-sqlite3 $DB_FILE < ../sql/schema/schema.sqlite.sql 
-sqlite3 $DB_FILE < ../sql/data/default_data.sql 
-sqlite3 $DB_FILE < ../sql/data/sample_data.sql 
-sqlite3 $DB_FILE < ../sql/data/test_data.sql 
+sqlite3 $DB_FILE < sql/schema/schema.sqlite.sql 
+sqlite3 $DB_FILE < sql/data/default_data.sql 
+sqlite3 $DB_FILE < sql/data/sample_data.sql 
+sqlite3 $DB_FILE < sql/data/test_data.sql 
 chmod 777 $DB_FILE
