d9f593fee9ae3b1df82c0dbda07cf225670f0c0e
wgreen <wgreen@d21c1205-ac1a-0410-ae1a-9dc866783d85>
2009-03-28 16:28:06 +0000

    Initial migration for Postgres. Has had basic testing.

--- sql/migration/3/migration.postgres.sql
+++ sql/migration/3/migration.postgres.sql
@@ -5,4 +5,42 @@
 -- Update Postgres database schema to v3                                    --
 -- ---------------------------------------------------------------------------
 
--- Add NOT NULL for all fk
+BEGIN;
+
+-- Set NOT NULL for all foreign key constraints, this was missed in the RackMonkey v1.2.3 schema
+-- Because there wasn't an automated migration from RackMonkey v1.2.3 to v1.2.4 this may also be needed for v1.2.4 users
+ALTER TABLE room ALTER COLUMN building SET NOT NULL;
+ALTER TABLE row ALTER COLUMN room SET NOT NULL;
+ALTER TABLE rack ALTER COLUMN row SET NOT NULL;
+ALTER TABLE os ALTER COLUMN manufacturer SET NOT NULL;
+ALTER TABLE hardware ALTER COLUMN manufacturer SET NOT NULL;
+ALTER TABLE device ALTER COLUMN domain SET NOT NULL;
+ALTER TABLE device ALTER COLUMN rack SET NOT NULL;
+ALTER TABLE device ALTER COLUMN hardware SET NOT NULL;
+ALTER TABLE device ALTER COLUMN os SET NOT NULL;
+ALTER TABLE device ALTER COLUMN customer SET NOT NULL;
+ALTER TABLE device ALTER COLUMN service SET NOT NULL;
+ALTER TABLE device ALTER COLUMN role SET NOT NULL;
+-- NOT NULL is set for the device_app table when it's created below
+
+-- Add additional columns
+ALTER TABLE rack ADD COLUMN numbering_direction INTEGER DEFAULT 0;
+ALTER TABLE device ADD COLUMN os_licence_key VARCHAR;
+
+DROP TABLE device_app;
+CREATE TABLE device_app
+(
+    id SERIAL PRIMARY KEY,
+	app INTEGER NOT NULL REFERENCES app,
+	device INTEGER NOT NULL REFERENCES device,
+	relation INTEGER NOT NULL REFERENCES app_relation,
+	meta_update_time VARCHAR,
+	meta_update_user VARCHAR
+);
+
+UPDATE rm_meta SET value='%BUILD%' WHERE name='system_build';
+UPDATE rm_meta SET value='3' WHERE name='schema_version';
+
+COMMIT;
+
+VACUUM;
